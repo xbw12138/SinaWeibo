783540d7395055ee480d516e98ad12d8f207d566
BadApple <1076351865@qq.com>
2017-04-21 18:45:24 +0800

    增加ssh

---
+++ weibo/ssh.php
@@ -0,0 +1,28 @@
+<?php
+	$host=$_POST['host'];
+	$ports=$_POST['port'];
+	$ssh_username="root";
+	$ssh_password="password";
+	$command="/home/ss.sh";
+	var_dump(ssh_exec($host, $ports, $ssh_username, $ssh_password, $command));
+	function ssh_exec($host, $port, $ssh_username, $ssh_password, $command) {  
+		$con = ssh2_connect($host, $port);  
+		$auth_methods = ssh2_auth_none($con, $ssh_username);  
+		if (in_array('password', $auth_methods)) {//是否允许使用密码登陆  
+			$auth_methods = ssh2_auth_password($con, $ssh_username, $ssh_password);  
+		}  
+		$stdout_stream = ssh2_exec($con, $command);  
+		$err_stream = ssh2_fetch_stream($stdout_stream, SSH2_STREAM_STDERR);  
+	  
+		stream_set_blocking($stdout_stream, true); //阻塞执行  
+		stream_set_blocking($err_stream, true);  
+	  
+		$result_stdout = stream_get_contents($stdout_stream);  
+		$result_error = stream_get_contents($err_stream);  
+	  
+		fclose($stdout_stream);  
+		fclose($err_stream);  
+		return array('result' => $result_stdout, 'error' => $result_error);  
+		
+	} 
+?>
